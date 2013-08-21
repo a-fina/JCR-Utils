e81aa08d745ce04a8104d43d0096e909d22b7d85
afinamore <afinamore@192.168.1.122>
2013-08-21 15:49:55 +0200

    Maven project , Connect JCR app

---
+++ JCR-Utils/src/main/java/al76/net/jcr/utils/App.java
@@ -0,0 +1,32 @@
+package al76.net.jcr.utils;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.jcr.Credentials;
+import javax.jcr.LoginException;
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
+import org.apache.jackrabbit.commons.JcrUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	public static void main(String[] args) {
+		try {
+			Repository repos = JcrUtils.getRepository("http://localhost:4502/crx/server");
+
+			Credentials cd = new SimpleCredentials("admin", "admin".toCharArray());
+
+			Session session = repos.login(cd);
+
+			System.out.println("Hello Session UserID: " + session.getUserID());
+		} catch (RepositoryException ex) {
+			Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+}
